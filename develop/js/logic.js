7aec789b1bc74d8689168d3b15c3e6c9b02d91ba
danielsantana9824 <danielsantana9824@gmail.com>
2024-09-07 22:49:31 -0500

    9/7/24 rigth side
    i put he first position with it information

--- develop/js/logic.js
+++ develop/js/logic.js
@@ -1,13 +1,14 @@
 const id = "2172797";
 const city = "Louisa";
+const apeKey = "";
 
-// const test = $('#data');
+const titleId = document.getElementById("title");
+const tempId = document.getElementById("temp");
+const windId = document.getElementById("wind");
+const humidityId = document.getElementById("humidity");
 
-const test = document.getElementById("#data");
 
-
-
-const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city},id=${id}&cnt=6&appid=`;
+const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city},id=${id}&cnt=6&appid=${apeKey}`;
 
 function save() {
     console.log("heree");
@@ -22,16 +23,45 @@ function getInformation() {
             return response.json();
         })
         .then(function (data) {
-            // console.log(data);
-             let ver = data.list;
 
-             for (let i = 0; i < ver.length; i++) {
-                console.log(ver[i].wind.speed);
-                
-                
-             }
-        
-        console.log(ver);
+            const date = dayjs().format('(MM/DD/YYYY)');
+
+            let city = data.city.name;
+            let wheaterDay = data.list[0];
+
+            let title = city + " " + date;
+
+            let temp = "Temp: "+wheaterDay.main.temp+" °F";
+
+            let wind = "Wind: "+wheaterDay.wind.speed+" MPH";
+
+            let humidity = "Humidity: "+wheaterDay.main.humidity+" %";
+
+            const titleEl = document.createElement("h1");
+            titleEl.textContent = title;
+            titleId.appendChild(titleEl);
+
+            const tempEl = document.createElement("p");
+            tempEl.textContent = temp;
+            tempId.appendChild(tempEl);
+
+            const windEl = document.createElement("p");
+            windEl.textContent = wind;
+            windId.appendChild(windEl);
+
+            const humidityEl = document.createElement("p");
+            humidityEl.textContent = humidity;
+            humidityId.appendChild(humidityEl);
+
+            let ver = data.list;
+
+            for (let i = 1; i < data.list.length; i++) {
+               console.log(data.list[i]);
+               
+
+            }
+
+            // console.log(ver);
 
         });
 }
