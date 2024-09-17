ec727ab60f74c4abed7806abfa6325451a592f5b
danielsantana9824 <danielsantana9824@gmail.com>
2024-09-16 20:18:57 -0500

    9/16/24 i created card
    i created dinamicly cards with information i got to api

--- develop/js/logic.js
+++ develop/js/logic.js
@@ -1,11 +1,14 @@
 const id = "2172797";
 const city = "Louisa";
-const apeKey = "";
+const apeKey = "65c7600db9ad1618aa7165b36f2c4a6c";
 
 const titleId = document.getElementById("title");
 const tempId = document.getElementById("temp");
 const windId = document.getElementById("wind");
 const humidityId = document.getElementById("humidity");
+const cardId = document.querySelector(".cards");
+const daysTemp = document.querySelector('.days');
+
 
 
 const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city},id=${id}&cnt=6&appid=${apeKey}`;
@@ -31,11 +34,13 @@ function getInformation() {
 
             let title = city + " " + date;
 
-            let temp = "Temp: "+wheaterDay.main.temp+" °F";
+            let tempCal = tempCalculo(wheaterDay.main.temp);
 
-            let wind = "Wind: "+wheaterDay.wind.speed+" MPH";
+            let temp = `Temp: ${tempCal.toFixed(2)} °F`;
 
-            let humidity = "Humidity: "+wheaterDay.main.humidity+" %";
+            let wind = "Wind: " + wheaterDay.wind.speed + " MPH";
+
+            let humidity = "Humidity: " + wheaterDay.main.humidity + " %";
 
             const titleEl = document.createElement("h1");
             titleEl.textContent = title;
@@ -53,17 +58,47 @@ function getInformation() {
             humidityEl.textContent = humidity;
             humidityId.appendChild(humidityEl);
 
-            let ver = data.list;
+
 
             for (let i = 1; i < data.list.length; i++) {
-               console.log(data.list[i]);
-               
 
+                let temp = tempCalculo(data.list[i].main.temp);
+
+                let fechaHoy = new Date();
+                let fechaSumada = sumarDias(fechaHoy, i);
+
+                daysTemp.innerHTML += createCard(fechaSumada, temp.toFixed(2), data.list[i].wind.speed, data.list[i].main.humidity);
             }
-
-            // console.log(ver);
 
         });
 }
 
 getInformation();
+
+function createCard(fechaSumada, temp, wind, humidity) {
+
+    return `
+         <div class="col-sm-4 bottom">
+             <div class="card bg-primary text-white">
+                 <div class="card-body">
+                     <h4 class="card-title">${fechaSumada}</h4>
+                     <p class="card-text">Temp: ${temp}</p>
+                    <p class="card-text">Wind: ${wind}</p>
+                    <p class="card-text">Humidity: ${humidity} %</p>
+                 </div>
+             </div>
+         </div>
+     `;
+}
+
+function tempCalculo(temp) {
+    let FandC = (temp - 273.15) * 9 / 5 + 32;
+    return FandC;
+}
+
+
+function sumarDias(fecha, dias) {
+    let nuevaFecha = new Date(fecha);
+    nuevaFecha.setDate(nuevaFecha.getDate() + dias);
+    return nuevaFecha.toLocaleDateString('en-US');;
+}
